014838c5cfa5ad937192a66ce03a2e68ba64b3ba
Ben Love <benuklove@users.noreply.github.com>
2016-01-31 20:44:08 -0600

    ProjectEuler Problems Solved
    Finds and returns number of circular primes below one million.
    Modified isprime function to be limited to factors below sqrt(n) -
    speeds up considerably.  Sieve of Eratosthenes not really needed here.

--- Practice/problem35.py
+++ Practice/problem35.py
@@ -5,10 +5,29 @@ Spyder Editor
 This is a temporary script file.
 """
 
+from math import sqrt
+
 print "Enter the limit: "
 num = int(raw_input())
 
 # returns 1 if number is prime, otherwise 0
+def isprime(n):
+    flag = 0
+    if n == 0 or n == 1 or n % 2 == 0:
+        return 0
+    if n == 2 or n == 3:
+        return 1
+    end = int(sqrt(n)) + 1
+    for x in range(3, end, 2):
+        #print x
+        if n % x == 0:
+            flag += 1
+            break
+    if flag == 0:
+        return 1
+    else:
+        return 0
+"""
 def isprime(n):
     flag = 0
     if n % 2 == 0:
@@ -21,8 +40,9 @@ def isprime(n):
         return 1
     else:
         return 0
-
+"""
 # given a string, bring the last character to the front (rotate once)
+# and return the number (as an int)
 def rotate(n):
     old = list(n)
     last = old[len(old) - 1]
@@ -39,19 +59,24 @@ def allrotsprime(n):
     length = len(numstr)
     flag = 0
     for x in range(length):
-        if isprime(rotate(numstr)) == 0:
+        numstr = rotate(str(numstr))
+        #print numstr
+        if isprime(numstr) == 0:
             flag += 1
+            #print "not prime: ", numstr
     if flag == 0:
         return 1
     else:
         return 0
+#allrotsprime(986497)
 
 #given an upper limit, find total number of circular primes below limit
 def circularprimes(n):
     count = 0
-    for x in range(2, n):
+    for x in range(3, n, 2):
         if isprime(x) == 1:
             if allrotsprime(x) == 1:
+                print x
                 count += 1
     return count + 1
 
